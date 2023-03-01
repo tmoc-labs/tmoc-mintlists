b5da1a68d60879679c3f84153e1ddcf05a448e21
tmoc <michael@tmoclabs.com>
2023-03-02 00:47:51 +0800

    Updating detect-minor-change.sh

--- bin/detect-minor-change.sh
+++ bin/detect-minor-change.sh
@@ -5,11 +5,10 @@ for file in $(git diff --name-only --diff-filter=d HEAD~1 HEAD | grep "^src/mint
   # Check if the file exists in both commits
   if git diff --name-only HEAD~1 HEAD | grep -q "$file"; then
     # Compare the mints arrays in both versions of the file
-    removed_mints=$(jq -r --argjson old "$(git show HEAD~1:"$file")" --argjson new "$(git show HEAD:"$file")" '
-      $old.mints as $old
-      | $new.mints as $new
-      | reduce $old[] as $o ([]; if ($o | IN($new[])) then . else . + [$o] end)
-    ')
+    removed_mints=$(comm -13 \
+      <(jq -r '.mints | sort[]' <(git show HEAD:"$file")) \
+      <(jq -r '.mints | sort[]' <(git show HEAD~1:"$file"))
+    )
 
     # Output any removed mints
     if [ -n "$removed_mints" ]; then
